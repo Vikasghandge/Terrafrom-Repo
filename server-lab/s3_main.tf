7078f7e5811e32b14100caacf386cee4f17a1b98
vikasghandge <ghandgevikas804@gmail.com>
2024-08-08 23:45:38 +0530

    --- Auto Git Commit ---

--- server-lab/s3_main.tf
+++ server-lab/s3_main.tf
@@ -1,10 +1,15 @@
+# Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1"  # AWS region where the S3 bucket will be created
 }
+
+# Define the S3 bucket resource
 resource "aws_s3_bucket" "my_bucket" {
-  bucket  = "my-unique-bucket-name"
-  tags    = {
-	Name          = "MyS3Bucket"
-	Environment    = "Production"
+  bucket = "my-unique-bucket-name"  # Replace with a globally unique bucket name
+  acl    = "private"  # Set bucket ACL to private by default
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = "Production"
   }
 }
