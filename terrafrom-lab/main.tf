4cb4e24809b3bfbd865ec3d4a4505afd1a697c81
vikasghandge <ghandgevikas804@gmail.com>
2024-08-08 22:24:40 +0530

    --- Auto Git Commit ---

--- terrafrom-lab/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "test_server" {
-     ami = "ami-0ad21ae1d0696ad58"
-     instance_type = "t2.micro"
-     tags = {
-        name = "examlewebserverapp"
-     }     }     
-}
