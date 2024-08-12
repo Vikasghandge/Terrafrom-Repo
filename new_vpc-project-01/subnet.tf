7ed0ff99f1df33e2db1217d42725eac9e352fd93
vikasghandge <ghandgevikas804@gmail.com>
2024-08-12 14:41:15 +0530

    --- Auto Git Commit ---

--- new_vpc-project-01/subnet.tf
+++ new_vpc-project-01/subnet.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south"
+  availability_zone       = "ap-south-1"
   map_public_ip_on_launch = true
 
   tags = {
