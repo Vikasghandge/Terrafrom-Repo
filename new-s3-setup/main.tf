25037c3dcaa837ebf384397c10c52187d26e4886
vikasghandge <ghandgevikas804@gmail.com>
2024-08-13 13:23:25 +0530

    --- Auto Git Commit ---

--- new-s3-setup/main.tf
+++ new-s3-setup/main.tf
@@ -1,22 +1,12 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
-  acl    = "public"
 
   tags = {
     Name = "Terraform state bucket"
   }
 }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = var.dynamodb_table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform lock table"
-  }
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
 }
