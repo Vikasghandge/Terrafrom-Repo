5c2995015bf5fc67b2dd0bdec6f0f8b12f8f84fa
vikasghandge <ghandgevikas804@gmail.com>
2024-08-13 12:46:06 +0530

    --- Auto Git Commit ---

---
+++ s3-project-setup/provider.tf
@@ -0,0 +1,15 @@
+#Provider for authentication
+provider "aws" {
+    region = var.region
+}
+
+# # Terraform for S3 Backend
+terraform {
+    backend "s3" {
+    bucket = "terraform-state-bucket-cdec-b37"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    dynamodb_table = "terraform_state_lock"
+    }
+}
+
