baf9dbb0bafb7430d17bca30084e457f93225447
vikasghandge <ghandgevikas804@gmail.com>
2024-08-14 18:37:22 +0530

    --- Auto Git Commit ---

--- ssh-keygen/keygen.tf
+++ ssh-keygen/keygen.tf
@@ -2,10 +2,7 @@ provider "aws" {
     region = "ap-south-1"
   
 }
-provider "aws" {
 
-  
-}
 #ec2 instance details
 resource "aws_instance" "this" {
     ami = "ami-0ad21ae1d0696ad58"
@@ -17,7 +14,7 @@ resource "aws_instance" "this" {
   resource "aws_key_pair" "aws_vikas" {
     key_name = "aws_vikas"
     public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIdAJ4nddhoYZ9iTyzuSoQtgTbnd1UQ4hiUVYFlD0PRu ubuntu@ip-172-31-2-98"
-    
+
 
 
     
