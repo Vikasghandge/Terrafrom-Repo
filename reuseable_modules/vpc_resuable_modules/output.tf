b32d999982bae509483127564650edf9d15c6342
Vikasghandge <ghandgevikas804@gmail.com>
2024-11-07 16:56:39 +0530

    Update output.tf

--- reuseable_modules/vpc_resuable_modules/output.tf
+++ reuseable_modules/vpc_resuable_modules/output.tf
@@ -1,1 +1,9 @@
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance"
+  value       = aws_instance.example_instance.public_ip
+}
 
+output "vpc_id" {
+  description = "The ID of the created VPC"
+  value       = aws_vpc.example_vpc.id
+}
