8af266a4c8d90b4edc59aafb77f08bf66deb28f7
Vikasghandge <ghandgevikas804@gmail.com>
2024-11-07 16:51:25 +0530

    Update main.tf

---
+++ reuseable_modules/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "ap-south-1"  # Set to ap-south-1 (Mumbai region)
+}
+
+resource "aws_instance" "example" {
+  ami           = var.ami_value              # AMI ID from variables file
+  instance_type = var.instance_type_value    # Instance type from variables file
+  subnet_id     = var.subnet_id_value        # Subnet ID from variables file
+}
