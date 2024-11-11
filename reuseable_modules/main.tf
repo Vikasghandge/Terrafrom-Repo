0fd8ea14e216860dbaf1e917b11214ee7cd3dd4f
vikasghandge <ghandgevikas804@gmail.com>
2024-11-11 19:19:12 +0530

    --- Auto Git Commit ---

--- reuseable_modules/main.tf
+++ reuseable_modules/main.tf
@@ -7,3 +7,13 @@ resource "aws_instance" "example" {
   instance_type = var.instance_type_value    # Instance type from variables file
   subnet_id     = var.subnet_id_value        # Subnet ID from variables file
 }
+
+terraform {
+  backend "s3" {
+    bucket = "bucccket2"
+    key    = "path/to/your/statefile.tfstate"  # Specify a path inside the bucket for your state file
+    region = "ap-south-1"                     # Replace with the AWS region of your bucket
+    encrypt = true                             # Optional, but recommended for encryption at rest
+  }
+}
+
