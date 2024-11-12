e088e96e7e690e3bda59c756709c3d85aab53a59
vikasghandge <ghandgevikas804@gmail.com>
2024-11-12 13:23:29 +0530

    --- Auto Git Commit ---

--- reuseable_modules/main.tf
+++ reuseable_modules/main.tf
@@ -8,5 +8,12 @@ resource "aws_instance" "example" {
   subnet_id     = var.subnet_id_value        # Subnet ID from variables file
 }
 
-
+terraform {
+  backend "s3" {
+    bucket = "bucccket2"                   # Your S3 bucket for Terraform state
+    key    = "terraform/state/statefile.tfstate"  # Path inside the bucket for your state file
+    region = "ap-south-1"                  # AWS region for the S3 bucket
+    encrypt = true                         # Optional, enables encryption at rest for the state file
+  }
+}
 
