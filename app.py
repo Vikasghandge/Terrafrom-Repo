140a10a8fd07e53eb2ed57480667fabdc7fc7f72
vikasghandge <ghandgevikas804@gmail.com>
2024-11-12 23:13:13 +0530

    --- Auto Git Commit ---

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ s3 = boto3.client ("s3")
 
 def lambda_handler(event, context):
     source_bucket = event["Records"][0]["s3"] ["bucket"]["name"]# type: ignore
-    object_key = event["Records"][0]["s3"] ["bucket"]["name"]
+    object_key = event["Records"][0]["s3"] ["bucket"]["key"]
+    
