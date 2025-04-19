197f5284953131fd4216bd8582f0d841cb0d5c9c
vikasghandge <ghandgevikas804@gmail.com>
2025-04-19 13:52:07 +0530

    --- Auto Git Commit ---

--- modules/eks-cluster/variables.tf
+++ modules/eks-cluster/variables.tf
@@ -1,23 +1,11 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
 variable "cluster_name" {
   default = "my-eks-cluster"
 }
 
+variable "region" {
+  default = "ap-south-1"
+}
+
 variable "node_instance_type" {
   default = "t3.medium"
-}
-
-variable "desired_capacity" {
-  default = 2
-}
-
-variable "max_capacity" {
-  default = 3
-}
-
-variable "min_capacity" {
-  default = 1
 }
