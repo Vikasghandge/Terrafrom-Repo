e7d5373ef285e2d95588431e5c8ff08af2dcdb25
vikasghandge <ghandgevikas804@gmail.com>
2025-04-19 14:38:22 +0530

    --- Auto Git Commit ---

--- modules/eks-cluster/provider.tf
+++ modules/eks-cluster/provider.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"  # Using the latest stable version
-    }
-  }
-}
-
-provider "aws" {
-  region = var.region
-}
+variable project {}
+variable desired_nodes {}
+variable max_nodes {}
+variable min_nodes {}
+variable node_instance_type {}
