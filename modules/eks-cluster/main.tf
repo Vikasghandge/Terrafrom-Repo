826eb4aa2b5b4914047cdbb2770ebb9c313b29d9
vikasghandge <ghandgevikas804@gmail.com>
2025-04-19 14:13:59 +0530

    --- Auto Git Commit ---

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -1,50 +1,72 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
-provider "aws" {
-  alias  = "use1"
-  region = var.region
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
 }
 
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
   cluster_name    = var.cluster_name
-  cluster_version = "1.29"
-  subnet_ids      = data.aws_subnets.default.ids
-  vpc_id          = data.aws_vpc.default.id
+  cluster_version = var.cluster_version
+
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
 
   eks_managed_node_groups = {
     default = {
-      instance_types = [var.node_instance_type]
-      desired_size   = 2
-      max_size       = 3
-      min_size       = 1
+      min_size     = var.node_group_min_size
+      max_size     = var.node_group_max_size
+      desired_size = var.node_group_desired_size
+
+      instance_types = [var.node_group_instance_type]
+      capacity_type  = "ON_DEMAND"
     }
   }
 
-  tags = {
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  node_security_group_additional_rules = {
+    ingress_self_all = {
+      description = "Node to node all ports/protocols"
+      protocol    = "-1"
+      from_port   = 0
+      to_port     = 0
+      type        = "ingress"
+      self        = true
+    }
+    egress_all = {
+      description      = "Node all egress"
+      protocol         = "-1"
+      from_port        = 0
+      to_port          = 0
+      type             = "egress"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+    }
   }
 }
--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,11 +1,19 @@
 output "cluster_name" {
-  value = module.eks.cluster_name
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
 }
 
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+output "kubeconfig_command" {
+  description = "Command to configure kubectl"
+  value       = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+}
+
+output "vpc_id" {
+  description = "VPC ID"
+  value       = module.vpc.vpc_id
 }
