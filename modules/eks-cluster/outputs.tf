205378c3d786309a71d9ee947aa01522fde1cb82
vikasghandge <ghandgevikas804@gmail.com>
2025-04-19 14:38:07 +0530

    --- Auto Git Commit ---

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,19 +1,19 @@
-output "cluster_name" {
-  description = "EKS cluster name"
-  value       = module.eks.cluster_name
-}
+# output "cluster_name" {
+#   description = "EKS cluster name"
+#   value       = module.eks.cluster_name
+# }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
+# output "cluster_endpoint" {
+#   description = "Endpoint for EKS control plane"
+#   value       = module.eks.cluster_endpoint
+# }
 
-output "kubeconfig_command" {
-  description = "Command to configure kubectl"
-  value       = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
-}
+# output "kubeconfig_command" {
+#   description = "Command to configure kubectl"
+#   value       = "aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name}"
+# }
 
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
-}
+# output "vpc_id" {
+#   description = "VPC ID"
+#   value       = module.vpc.vpc_id
+# }
