40d7951208fc53e02d956f42f33783943b1bfa5b
vikasghandge <ghandgevikas804@gmail.com>
2025-04-19 13:52:52 +0530

    --- Auto Git Commit ---

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,23 +1,7 @@
-# output "cluster_name" {
-#   value = module.eks.cluster_name
-# }
-
-# output "kubeconfig" {
-#   value = module.eks.kubeconfig
-# }
-
-# output "cluster_endpoint" {
-#   value = module.eks.cluster_endpoint
-# }
-
 output "cluster_name" {
   value = module.eks.cluster_name
 }
 
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
-
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+output "kubeconfig" {
+  value = module.eks.kubeconfig_filename
 }
