03aed5a44da311d2d693561232d5b129f57797c4
vikasghandge <ghandgevikas804@gmail.com>
2025-04-19 14:38:52 +0530

    --- Auto Git Commit ---

--- modules/eks-cluster/variables.tf
+++ modules/eks-cluster/variables.tf
@@ -1,41 +1,5 @@
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
-}
-
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = "my-eks-cluster"
-}
-
-variable "cluster_version" {
-  description = "Kubernetes version"
-  type        = string
-  default     = "1.28"
-}
-
-variable "node_group_instance_type" {
-  description = "Instance type for EKS node group"
-  type        = string
-  default     = "t3.medium"
-}
-
-variable "node_group_min_size" {
-  description = "Minimum number of nodes in node group"
-  type        = number
-  default     = 1
-}
-
-variable "node_group_max_size" {
-  description = "Maximum number of nodes in node group"
-  type        = number
-  default     = 3
-}
-
-variable "node_group_desired_size" {
-  description = "Desired number of nodes in node group"
-  type        = number
-  default     = 2
-}
+variable project {}
+variable desired_nodes {}
+variable max_nodes {}
+variable min_nodes {}
+variable node_instance_type {}
