bc384f26f5b98f414e0fcd4d5c2ab55eea6cb134
vikasghandge <ghandgevikas804@gmail.com>
2025-04-26 15:57:57 +0530

    --- Auto Git Commit ---

--- server-lab/34.groovy
+++ server-lab/34.groovy
@@ -1,25 +1,3 @@
 pipeline {
-    agent any
-    stages {
-        stage('Checkout') {
-            steps {
-                git 'your git repo uurl'
-            }
-        }
-        stage('Build') {
-            steps {
-                sh 'mvn clean install'
-            }
-        }
-        stage('Test') {
-            steps {
-                'location of your pom.xml file'
-            }
-        }
-        stage('Deploy') {
-            stpes {
-                sh 'echo "we are deploying in test env"'
-            }
-        }
-    }
+    
 }
