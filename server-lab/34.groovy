245dd346655a9d04ac84c20c46a7f08cba9dd5e5
vikasghandge <ghandgevikas804@gmail.com>
2025-04-26 16:28:07 +0530

    --- Auto Git Commit ---

--- server-lab/34.groovy
+++ server-lab/34.groovy
@@ -16,7 +16,7 @@ pipeline {
                 withCredentials([userpassword(credentialsId: dockerhub-credentials, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
                    
                    sh '''
-                    echo '$DOCKER_PASS' |  docker ulogin -u '$DOCKER_USER'
+                    echo '$DOCKER_PASS' |  docker login -u '$DOCKER_USER'
                         docker tag 
                         docker push image_name/vikasghandge   
                         '''   
