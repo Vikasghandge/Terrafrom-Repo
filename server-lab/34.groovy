1ccde60d41fd1ca18e2bfd0c2b31d8c43a836d81
vikasghandge <ghandgevikas804@gmail.com>
2025-04-26 16:04:29 +0530

    --- Auto Git Commit ---

--- server-lab/34.groovy
+++ server-lab/34.groovy
@@ -12,8 +12,8 @@ pipeline {
             }
         }
         stage('Docker Push') {
-            s {
-                withCredentials([userpassword(credentialsId: dockerhub-credentials, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+            steps {
+                withCredentials([userpassword(credentialsId: dockerhub-credentials, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) 
 
                 }
             }
