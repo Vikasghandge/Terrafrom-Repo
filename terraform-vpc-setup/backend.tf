d4b00686de28153aa6fbb1133287aef29326510a
vikasghandge <ghandgevikas804@gmail.com>
2025-04-26 13:07:53 +0530

    --- Auto Git Commit ---

--- terraform-vpc-setup/backend.tf
+++ terraform-vpc-setup/backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket"
+    bucket = "my-terraform-state-bucket123"
     key    = "vpc/terraform.tfstate"
     region = "ap-south-1"
   }
--- terraform-vpc-setup/variables.tf
+++ terraform-vpc-setup/variables.tf
@@ -3,7 +3,7 @@ variable "region" {
   default = "ap-south-1"
 }
 variable "bucket_name" {
-  default = "my-terraform-state-bucket"
+  default = "my-terraform-state-bucket123"
 }
 variable "ami" {
   default = "ami-02a2af70a66af6dfb"
