3501c497624a4f997096b9e74c415433ae6f614e
vikasghandge <ghandgevikas804@gmail.com>
2025-04-27 15:27:02 +0530

    --- Auto Git Commit ---

--- b37.groovy
+++ b37.groovy
@@ -20,7 +20,17 @@ pipeline {
             }
         }
 
-        
+        stage('SonarQube Analysis') {
+    steps {
+        withSonarQubeEnv('sonar') {
+            sh """
+            ${SCANNER_HOME}/bin/sonar-scanner \
+            -Dsonar.projectName=devops-exam-app \
+            -Dsonar.projectKey=devops-exam-app \
+            -Dsonar.sources=. \
+            -Dsonar.python.version=3 \
+            -Dsonar.host.url=http://localhost:9000
+            """
                 }
             }
         }
