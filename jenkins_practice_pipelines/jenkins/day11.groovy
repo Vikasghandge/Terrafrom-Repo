b0bc8c6cf2d68d790a269cea77d5ebe86f01241f
vikasghandge <ghandgevikas804@gmail.com>
2025-04-28 21:11:37 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day11.groovy
+++ jenkins_practice_pipelines/jenkins/day11.groovy
@@ -37,7 +37,7 @@ pipeline {
             steps {
                 script {
                     sh '''
-                    
+                    kubectl apply -f deployment
                     '''
                 }
             }
