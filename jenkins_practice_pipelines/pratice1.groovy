0666448439ea3808a433a3534ae14503da188766
vikasghandge <ghandgevikas804@gmail.com>
2025-04-28 13:15:13 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/pratice1.groovy
+++ jenkins_practice_pipelines/pratice1.groovy
@@ -13,7 +13,7 @@ pipeline {
         }
         stage('push image') {
             steps {
-                withCredentials[(userpassword(dockerhub-credentialsId: user))]
+                withCredentials[(userpassword(dockerhub-credentialsId: usernameVariable))]
             }
         }
     }
