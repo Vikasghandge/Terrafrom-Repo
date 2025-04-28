eb3f5f6d61902f8d344e9214e67e2c294021217f
vikasghandge <ghandgevikas804@gmail.com>
2025-04-28 20:47:10 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day11.groovy
+++ jenkins_practice_pipelines/jenkins/day11.groovy
@@ -1,7 +1,10 @@
 pipeline {
     agent any 
     stages {
-        stage('Checkout') 
-        
+        stage('Checkout') {
+            steps {
+                
+            }
+        }     
     }
 }
