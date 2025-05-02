c21ec1ae911fd52377195e65c21f0c49af5306bc
vikasghandge <ghandgevikas804@gmail.com>
2025-05-02 13:50:35 +0530

    --- Auto Git Commit ---

---
+++ jenkins_practice_pipelines/jenkins/test.groovy
@@ -0,0 +1,54 @@
+pipeline {
+    agent any
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('AWS_Access_Key')
+        AWS_SECRET_ACCESS_KEY = credentials('AWS_Secret_Key')
+        AWS_DEFAULT_REGION = 'ap-south-1'
+    }
+    stages{
+        stage('Clone the Code'){
+            steps{
+                script{
+                    checkout scmGit(branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/KastroVKiran/Jenkins-Terraform-EKS.git']])
+                }
+            }
+        }
+        stage('Terraform Initialization'){
+            steps{
+                script{
+                    dir('terraform'){
+                         sh 'terraform init'
+                    }
+                }
+            }
+        }
+        stage('Terraform Validation'){
+            steps{
+                script{
+                    dir('terraform'){
+                         sh 'terraform validate'
+                    }
+                }
+            }
+        }
+        stage('Infrastructure Checks'){
+            steps{
+                script{
+                    dir('terraform'){
+                         sh 'terraform plan'
+                    }
+                    input(message: "Approve?", ok: "proceed")
+                }
+            }
+        }
+        stage('Create/Destroy EKS cluster'){
+            steps{
+                script{
+                    dir('terraform'){
+                         sh 'terraform $action --auto-approve'
+                    }
+                }
+            }
+        }
+    }
+}
