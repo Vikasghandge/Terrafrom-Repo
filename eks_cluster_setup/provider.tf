dbc6a2c33f0c52650ac65b591cd11e19e050b42f
vikasghandge <ghandgevikas804@gmail.com>
2025-05-02 13:57:57 +0530

    --- Auto Git Commit ---

--- eks_cluster_setup/provider.tf
+++ eks_cluster_setup/provider.tf
@@ -1,16 +1,16 @@
 locals {
-  region = "ap-south-1"
-  name = "my-eks-cluster"
-  vpc_cidr = "10.0.0.0/16"
-  azs = ["ap-south-1a", ap-south-1b]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.3.0/24, "10.0.4.0/24"]
-  intra_subnets = ["10.0.5.0/24", "10.0.6.0/24]
+  region          = "ap-south-1"
+  name            = "my-eks-cluster"
+  vpc_cidr        = "10.0.0.0/16"
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  intra_subnets   = ["10.0.5.0/24", "10.0.6.0/24"]
   tags = {
-    Example = local.name
+    Example = "my-eks-cluster"
   }
 }
 
 provider "aws" {
-    region = "ap-south-1"
+  region = local.region
 }
