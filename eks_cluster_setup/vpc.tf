2a01906c294615ec8ef02bdfa739a111e0a3333f
vikasghandge <ghandgevikas804@gmail.com>
2025-05-02 14:01:48 +0530

    --- Auto Git Commit ---

--- eks_cluster_setup/vpc.tf
+++ eks_cluster_setup/vpc.tf
@@ -9,7 +9,7 @@ module "vpc" {
   azs = local.azs
   private_subnets = local.private_subnets
   public_subnets = local.public_subnets
-  infra_subnets = local.infra_subnets
+  intra_subnets = local.intra_subnets
 
   enable_nat_gateway = true 
   public_subnet_tags = {
