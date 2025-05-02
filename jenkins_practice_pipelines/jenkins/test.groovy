6bedf970a94eb4c2a462c4c016497dda20b8b7fe
vikasghandge <ghandgevikas804@gmail.com>
2025-05-02 13:51:25 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/test.groovy
+++ jenkins_practice_pipelines/jenkins/test.groovy
@@ -25,7 +25,7 @@ pipeline {
         stage('Terraform Validation'){
             steps{
                 script{
-                    dir('terraform'){
+                    dir('eks-cluster-setup'){
                          sh 'terraform validate'
                     }
                 }
