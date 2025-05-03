4e079d215da313a695ff6684e391f487ab6608d9
vikasghandge <ghandgevikas804@gmail.com>
2025-05-03 21:47:16 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/log_managmnt/delete_log.sh
+++ jenkins_practice_pipelines/log_managmnt/delete_log.sh
@@ -1,6 +1,6 @@
 find /path/of/your/logfile -type f -name "log.*" -mtime +7 -delete
 
-crontab - 0 0 * * * /
+crontab - 0 0 * * * /home/ubuntu/
 
 
 find /path/of/your/logfile -type f -name "log.*" -mtime +7 -delete
