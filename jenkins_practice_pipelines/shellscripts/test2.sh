e54a02e66c3221c06e1ef014bd6632f77c916db9
vikasghandge <ghandgevikas804@gmail.com>
2025-05-03 13:03:14 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/shellscripts/test2.sh
+++ jenkins_practice_pipelines/shellscripts/test2.sh
@@ -1,6 +1,6 @@
 SOURCE_DIR= "/home/ubuntu"  #this is source dir will copy from this dir
-DEST_DIR= "/tmp/backup_dir" #this is dest dir 
-DAYS_TO_KEEP=30
+DEST_DIR= "/tmp/backup_dir" #this is dest dir backup will be create here
+DAYS_TO_KEEP=30  #duration o
 DATE =$(date +"%y-%m-%d-%H-%M-%S")
 BACKUP_NAME= "$bakcup_$DATE.tar.gz"
 
