3001a609bcfe7b27cc5c808e7c845d49b99a06ef
vikasghandge <ghandgevikas804@gmail.com>
2025-05-03 12:44:49 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day4.4.groovy
+++ jenkins_practice_pipelines/jenkins/day4.4.groovy
@@ -11,7 +11,9 @@ pipeline {
             }
         }
         stage('Build Docker Image') {
-            
+            steps {
+                sh 'docker build -t $IMAGE_NAME:TAG .'
+            }
         }
     }
 }
