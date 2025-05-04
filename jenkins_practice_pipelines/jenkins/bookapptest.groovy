4147e56e2d2960dc8af8153f8e428cde844eae41
vikasghandge <ghandgevikas804@gmail.com>
2025-05-04 21:09:22 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/bookapptest.groovy
+++ jenkins_practice_pipelines/jenkins/bookapptest.groovy
@@ -72,7 +72,7 @@ pipeline {
                         docker build --no-cache -t ghandgevikas/bms:latest -f bookmyshow-app/Dockerfile bookmyshow-app
 
                         echo "Pushing Docker image to registry..."
-                        docker push kastrov/bms:latest
+                        docker push ghandgevikas/bms:latest
                         '''
                     }
                 }
