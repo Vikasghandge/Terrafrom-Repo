a22b3a2b19e26a25703b2c7b2ce5d529b9da8f4d
vikasghandge <ghandgevikas804@gmail.com>
2025-05-05 13:07:30 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day5.5.groovy
+++ jenkins_practice_pipelines/jenkins/day5.5.groovy
@@ -17,7 +17,7 @@ pipeline {
         stage('Docker Login') {
             steps {
                 withCredentials([usernamepassword(credentialsId: DOCKERHUB_CREDENTIALS, usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-                    sh 'echo $DOCKER_PASS | docker login -u $'
+                    sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER' --password
                 }
             }
         }
