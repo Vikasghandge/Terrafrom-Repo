5489308f2585425738693115d5db0b3b2eb75d6e
vikasghandge <ghandgevikas804@gmail.com>
2025-05-05 13:20:15 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/shellscripts/test5.sh
+++ jenkins_practice_pipelines/shellscripts/test5.sh
@@ -1,1 +1,1 @@
-# shell script for to take automated backup of my file for 30 days and older than 30days 
+# shell script for to take automated backup of my file for 30 days and older than 30days it will delete 
