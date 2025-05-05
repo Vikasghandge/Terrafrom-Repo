653f01a7f02118f6c5b079d19f3c512164ff2d0b
vikasghandge <ghandgevikas804@gmail.com>
2025-05-05 13:31:08 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/shellscripts/test5.sh
+++ jenkins_practice_pipelines/shellscripts/test5.sh
@@ -14,7 +14,7 @@ tar -cvf "$DEST_DIR/$BACKUP_NAME" -C "$SOURCE_DIR"
 
 find $DEST_DIR -type f -name "backup_*.tar.gz" -mtime +30 -delete
 
-echo "backup has taken sucessfully: $BACKUP"
+echo "backup has taken sucessfully: $BACKUP_NAME"
 
 
 
