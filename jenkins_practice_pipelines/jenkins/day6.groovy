b87dd7ca3701d0b243dea2a0b75a2c9394cb5810
vikasghandge <ghandgevikas804@gmail.com>
2025-05-06 12:49:28 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day6.groovy
+++ jenkins_practice_pipelines/jenkins/day6.groovy
@@ -3,8 +3,9 @@ pipeline {
     stages{
         stage('Checkout') {
             steps {
-                git url: 'https://github.com/your-user/your-repo.git'
+                git url: 'https://github.com/your-user/your-repo.git', branch:'main'
             }
         }
+        stage()
     }
 }
