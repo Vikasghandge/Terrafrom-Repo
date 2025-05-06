7711a7c600bec1cdf720ac52367bf57c0c60f9a8
vikasghandge <ghandgevikas804@gmail.com>
2025-05-06 13:24:07 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day6.6.groovy
+++ jenkins_practice_pipelines/jenkins/day6.6.groovy
@@ -17,7 +17,7 @@ pipeline {
         }
         stage('Docker Login Hub') {
             steps {
-                withCredentials([])
+                withCredentials([usernamepassword])
             }
         }
     }
