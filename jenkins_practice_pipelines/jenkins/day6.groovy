f5d886d28647a6f52a46a940c1228ba37239d57e
vikasghandge <ghandgevikas804@gmail.com>
2025-05-06 12:53:59 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day6.groovy
+++ jenkins_practice_pipelines/jenkins/day6.groovy
@@ -18,7 +18,7 @@ pipeline {
         }
         stage('Deploy') {
             steps {
-                sh "Deplo"
+                sh "Deploying in local environment"
             }
         }
     }
