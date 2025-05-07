e406c400d26b7a85200c3a1148543af77ab1ed1d
vikasghandge <ghandgevikas804@gmail.com>
2025-05-07 13:48:45 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day7.7.groovy
+++ jenkins_practice_pipelines/jenkins/day7.7.groovy
@@ -11,7 +11,7 @@
         stages {
             stage('Chekout code') {
                 steps {
-                    'branch: n'
+                    'branch: main url:'
                 }
             }
         }
