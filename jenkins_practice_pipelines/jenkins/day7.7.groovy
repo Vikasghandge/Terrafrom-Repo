336fc67fb56021f163f16f9f0b4936502a09ddf8
vikasghandge <ghandgevikas804@gmail.com>
2025-05-07 14:04:10 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day7.7.groovy
+++ jenkins_practice_pipelines/jenkins/day7.7.groovy
@@ -28,7 +28,7 @@
             }
             stage('Docker Image Push registery') {
                 steps {
-                    sh 'docker push '$IMAGE_NAME'
+                    sh 'docker push '$IMAGE_NAME':'$TAG''
                 }
             }
         }
