a38fbd5983d13344760a08dae215aaaf1bd1e5fb
vikasghandge <ghandgevikas804@gmail.com>
2025-05-07 14:16:24 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/shellscripts/serrunning.sh
+++ jenkins_practice_pipelines/shellscripts/serrunning.sh
@@ -24,4 +24,13 @@ if systemctl is-active --quite "$SERIVICE" then;
     echo "$SERVICE is running"
 else 
     echo "$SERVICE is not running"
-fi        
+fi       
+
+######################################################
+
+SERVICE='nginx'
+ if systemctl is-active --quite '$SERVICE' then;
+ echo '$SERVICE is running' 
+ else  
+    echo '$SERVICE is not running'
+fi
