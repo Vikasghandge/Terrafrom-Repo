c29521712ac3cb7fffb4a9b29a77e9d8e94dd982
vikasghandge <ghandgevikas804@gmail.com>
2025-05-07 14:26:17 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/shellscripts/test6.sh
+++ jenkins_practice_pipelines/shellscripts/test6.sh
@@ -8,4 +8,6 @@ mkdir -p $DEST_DIR
 
 tar -czf "$DEST_DIR/$BACKUP_NAME", -C "$SOURCE_DIR"
 
-find $DEST_DIR -type -f -name "backup_*.tar.gz" -mtime DAYS_TO_KEEP
+find $DEST_DIR -type -f -name "backup_*.tar.gz" -mtime +DAYS_TO_KEEP -delete
+
+
