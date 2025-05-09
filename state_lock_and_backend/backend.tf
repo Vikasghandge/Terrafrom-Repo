b44fa0914bc55f1f010575052368a97f5b6841c0
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 15:42:04 +0530

    --- Auto Git Commit ---

--- state_lock_and_backend/backend.tf
+++ state_lock_and_backend/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "demo1122233bucket"
-    key = "vikas/state.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "terraform-lock"
-    encrypt = true
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "demo1122233bucket"
+#     key = "vikas/state.tfstate"
+#     region = "ap-south-1"
+#     dynamodb_table = "terraform-lock"
+#     encrypt = true
+#   }
+# }
