1bfba30a7e909b23cedf00e7fccd2d83ab1eea73
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 23:24:44 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/bookapptest.groovy
+++ jenkins_practice_pipelines/jenkins/bookapptest.groovy
@@ -91,3 +91,83 @@ pipeline {
         }
     }
 }
+
+
+
+---------------------------------------------------------------------------------------
+===================================================================================
+
+pipeline{
+    agent any
+    tools{
+        jdk 'jdk17'
+        nodejs 'node19'
+    }
+    environment {
+        SCANNER_HOME=tool 'sonar-scanner'
+    }
+    stages {
+        stage('Checkout from Git'){
+            steps{
+                git branch: 'main', url: 'https://github.com/Vikasghandge/Terrafrom-Repo.git'
+            }
+        }
+        stage('Install Dependencies') {
+            steps {
+                sh "npm install"
+            }
+        }
+        stage("Sonarqube Analysis "){
+            steps{
+                withSonarQubeEnv('sonar-server') {
+                    sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=Chatbot \
+                    -Dsonar.projectKey=Chatbot '''
+                }
+            }
+        }
+        stage("quality gate"){
+           steps {
+                script {
+                    waitForQualityGate abortPipeline: false, credentialsId: 'Sonar-token'
+                }
+            }
+        }
+        stage('OWASP FS SCAN') {
+            steps {
+                dependencyCheck additionalArguments: '--scan ./ --disableYarnAudit --disableNodeAudit', odcInstallation: 'DP-Check'
+                dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+            }
+        }
+        stage('TRIVY FS SCAN') {
+            steps {
+                sh "trivy fs . &gt; trivyfs.json"
+            }
+        }
+        stage("Docker Build &amp; Push"){
+            steps{
+                script{
+                   withDockerRegistry(credentialsId: 'docker', toolName: 'docker'){
+                       sh "docker build -t chatbot ."
+                       sh "docker tag chatbot sevenajay/chatbot:latest "
+                       sh "docker push sevenajay/chatbot:latest "
+                    }
+                }
+            }
+        }
+        stage("TRIVY"){
+            steps{
+                sh "trivy image sevenajay/chatbot:latest &gt; trivy.json"
+            }
+        }
+        stage ("Remove container") {
+            steps{
+                sh "docker stop chatbot | true"
+                sh "docker rm chatbot | true"
+             }
+        }
+        stage('Deploy to container'){
+            steps{
+                sh 'docker run -d --name chatbot -p 3000:3000 sevenajay/chatbot:latest'
+            }
+        }
+    }
