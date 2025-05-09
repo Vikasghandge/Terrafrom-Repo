798a377fb8054fef0a204ce1604bb0b6c3c57f2c
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 15:48:26 +0530

    --- Auto Git Commit ---

--- state_lock_and_backend/main.tf
+++ state_lock_and_backend/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my-instance" {
     ami = "ami-0e35ddab05955cf57"
     instance_type = "t2.small"
-    key_name = "vikas.key"
+    key_name = "vikas-key"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
