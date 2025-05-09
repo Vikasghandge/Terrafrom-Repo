0dff09313ad354c366eda9104c6bad661d8d5171
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 16:07:01 +0530

    --- Auto Git Commit ---

--- state_lock_and_backend/main.tf
+++ state_lock_and_backend/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance" "my-instance" {
+resource "aws_instance" "my-instance11" {
     ami = "ami-0e35ddab05955cf57"
     instance_type = "t2.small"
     key_name = "vikas-key"
