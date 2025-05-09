637ca9f6dc78f6166b7ac50b96b5c3b5da30c6da
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 09:48:30 +0530

    --- Auto Git Commit ---

--- terraform_real_ec2_infra/main.tf
+++ terraform_real_ec2_infra/main.tf
@@ -2,57 +2,22 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_instance" "myapp_updated_instance" {
-  count                        = var.instance_count
-  ami                          = var.ami_id
-  instance_type                = var.instance_type
-  subnet_id                    = var.subnet_id
-  associate_public_ip_address  = true
-  vpc_security_group_ids       = [var.security_group_id]
+resource "aws_instance" "Terraform_test" {
+  count                       = var.instance_count
+  ami                         = var.ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = var.subnet_id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [var.security_group_id]
 
   tags = {
-    Name = "${var.instance_name}-${count.index + 1}"
+    Name = "${var.name_prefix}-${count.index}"
   }
 
   root_block_device {
     volume_size = 8
     volume_type = "gp3"
-    iops        = 3000
-    throughput  = 125
   }
 
-  credit_specification {
-    cpu_credits = "standard"
-  }
-
-  capacity_reservation_specification {
-    capacity_reservation_preference = "open"
-  }
-
-  cpu_options {
-    core_count       = 1
-    threads_per_core = 1
-  }
-
-  enclave_options {
-    enabled = false
-  }
-
-  maintenance_options {
-    auto_recovery = "default"
-  }
-
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_protocol_ipv6          = "disabled"
-    http_put_response_hop_limit = 2
-    http_tokens                 = "required"
-    instance_metadata_tags      = "disabled"
-  }
-
-  private_dns_name_options {
-    enable_resource_name_dns_a_record    = false
-    enable_resource_name_dns_aaaa_record = false
-    hostname_type                        = "ip-name"
-  }
+  # cpu_options block removed to support t2.micro
 }
