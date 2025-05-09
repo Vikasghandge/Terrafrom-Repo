8251080d962eea3a053935dce7ebc11693be343c
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 09:49:00 +0530

    --- Auto Git Commit ---

--- terraform_real_ec2_infra/variables.tf
+++ terraform_real_ec2_infra/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region to deploy the instance"
   type        = string
   default     = "ap-south-1"
 }
@@ -7,35 +7,32 @@ variable "aws_region" {
 variable "ami_id" {
   description = "AMI ID"
   type        = string
-  default     = "ami-0e35ddab05955cf57"
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Instance type"
   type        = string
   default     = "t2.micro"
-}
-
-variable "instance_count" {
-  description = "Number of EC2 instances"
-  type        = number
-  default     = 1
-}
-
-variable "instance_name" {
-  description = "EC2 instance name prefix"
-  type        = string
-  default     = "Terraform_test"
 }
 
 variable "subnet_id" {
   description = "Subnet ID"
   type        = string
-  default     = "subnet-0c0645d5bb4363e74"
 }
 
 variable "security_group_id" {
   description = "Security Group ID"
   type        = string
-  default     = "sg-08cabcfd4b939f93f"
+}
+
+variable "name_prefix" {
+  description = "Prefix for instance names"
+  type        = string
+  default     = "Terraform_test"
+}
+
+variable "instance_count" {
+  description = "Number of instances to launch"
+  type        = number
+  default     = 1
 }
