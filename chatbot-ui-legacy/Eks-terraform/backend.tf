4f35b79bd145ab3eb2f2194a1c37e079b8fa6a7a
Vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 22:43:51 +0530

    Update backend.tf

--- chatbot-ui-legacy/Eks-terraform/backend.tf
+++ chatbot-ui-legacy/Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postgressbuckett" # Replace with your actual S3 bucket name
+    bucket = "demo1122233buckettt3" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
