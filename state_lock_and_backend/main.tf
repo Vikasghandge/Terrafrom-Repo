8d0fe61a90812594f26872af56b9eae731895edc
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 16:06:31 +0530

    --- Auto Git Commit ---

--- state_lock_and_backend/main.tf
+++ state_lock_and_backend/main.tf
@@ -13,13 +13,13 @@ resource "aws_instance" "my-instance" {
 
 #}
 
-# resource "aws_dynamodb_table" "terraform_lock" {
-#     name = "terraform-lock"
-#     billing_mode = "PAY_PER_REQUEST"
-#     hash_key = "LockID"
+resource "aws_dynamodb_table" "terraform_lock" {
+    name = "terraform-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
     
-#     attribute {
-#       name = "LockID"
-#       type = "S"
-#     }
-# }
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
