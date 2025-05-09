49d211ffe1c4da0e530b71fb60944a0da7c1b577
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 09:56:02 +0530

    --- Auto Git Commit ---

--- terraform_real_ec2_infra/main.tf
+++ terraform_real_ec2_infra/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "Terraform_test" {
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   vpc_security_group_ids      = [var.security_group_id]
-  key_name = var.
+  key_name = var.key_name
 
   tags = {
     Name = "${var.name_prefix}-${count.index}"
