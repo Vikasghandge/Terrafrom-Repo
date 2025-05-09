63bbb4ab9465c82bbb0a0a81b04a97a6f8bae15f
vikasghandge <ghandgevikas804@gmail.com>
2025-05-09 15:30:21 +0530

    --- Auto Git Commit ---

--- state_lock_and_backend/main.tf
+++ state_lock_and_backend/main.tf
@@ -15,6 +15,7 @@ resource "aws_s3_bucket" "my_bucket" {
 
 resource "aws_dynamodb_table" "terraform_lock" {
     name = "terraform-lock"
-    billing_mode = "PAY_PER"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "value"
     
 }
