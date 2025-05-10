68460da40143afc379b7b3f0274981abb120884f
vikasghandge <ghandgevikas804@gmail.com>
2025-05-10 13:06:40 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day8.terraform.groovy
+++ jenkins_practice_pipelines/jenkins/day8.terraform.groovy
@@ -36,7 +36,7 @@ pipeline {
         stage('Terraform Apply/Destroy') {
             steps {
                 dir('terraform') {
-                    sh 'terraform ${action} --auto-approve'  // 
+                    sh 'terraform ${action} --auto-approve'  // execute actual chnages //
                 }
             }
         }
