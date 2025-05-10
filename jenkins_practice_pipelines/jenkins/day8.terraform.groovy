d3524a8abaabe0b981777d48d232c74d2ab12ea6
vikasghandge <ghandgevikas804@gmail.com>
2025-05-10 12:57:38 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day8.terraform.groovy
+++ jenkins_practice_pipelines/jenkins/day8.terraform.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Chekout Code') {
             steps {
-                'branch: main', url: 'http'
+                'branch: main', url: 'http://github.com/ghanadge'
             }
         }
     }
