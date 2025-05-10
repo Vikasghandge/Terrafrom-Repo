08dbc1ed15a7220b8b27d036ddd9b13efd804f22
vikasghandge <ghandgevikas804@gmail.com>
2025-05-10 13:19:04 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/shellscripts/test10.sh
+++ jenkins_practice_pipelines/shellscripts/test10.sh
@@ -1,7 +1,7 @@
 
 SOURCE_DIR="/home/ubuntu"   # source dir
 DEST_DIR="/tmp/backup_dir"  # destination dir
-DATE=(date +"%y-%m-%d-%H-%M-%S") 
-DAYS_TO_KEEP= 30
+DATE=(date +"%y-%m-%d-%H-%M-%S") # date and time
+DAYS_TO_KEEP= 30 
 BACKUP_NAME="backup_*.tar.gz"
 
