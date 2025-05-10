fc4c1e1706d709794e825b513a3b75f7dec1f78d
vikasghandge <ghandgevikas804@gmail.com>
2025-05-10 12:45:54 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day8.8.groovy
+++ jenkins_practice_pipelines/jenkins/day8.8.groovy
@@ -10,7 +10,7 @@ pipeline {
         stages {
             stage('Checkout Code') {
                 steps {
-                    'branch: main', url:'http://github.com/vikas'
+                    'branch: main', url:'http://github.com/vikasghandge'
                 }
             }
         }
