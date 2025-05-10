f8f156c335c85834c9f5b5c1a4f587375e06c650
vikasghandge <ghandgevikas804@gmail.com>
2025-05-10 12:49:25 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day8.8.groovy
+++ jenkins_practice_pipelines/jenkins/day8.8.groovy
@@ -21,7 +21,7 @@ pipeline {
         }
         stage('Docker Login') {
             steps {
-                withCredentilas([usernamePassword(CredentialsId:)])
+                withCredentilas([usernamePassword(CredentialsId: 'DOCKERHUB_')])
             }
         }
     }
