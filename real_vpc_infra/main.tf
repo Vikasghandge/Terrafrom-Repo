140ac25b2d9265bf60d464c175a93ed2e3ba3b48
vikasghandge <ghandgevikas804@gmail.com>
2025-05-10 20:43:56 +0530

    --- Auto Git Commit ---

--- real_vpc_infra/main.tf
+++ real_vpc_infra/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "Terraform_vpc" {
     # // this will help to import vpc manually crated //
 }
 
-resource "aws_subnet" "name" {
+resource "aws_subnet" "Terraform_" {
     vpc_id = "value"
     
 }
