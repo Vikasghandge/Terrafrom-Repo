f1b77b91dd2991e8e43dd92ceef9e7e11bacd5a7
vikasghandge <ghandgevikas804@gmail.com>
2025-05-11 18:49:50 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day9tarraform.groovy
+++ jenkins_practice_pipelines/jenkins/day9tarraform.groovy
@@ -12,7 +12,11 @@ pipeline {
                 sh 'terraform init'
             }
         }
-        stage('')
+        stage('Terraform validate') {
+            steps {
+                
+            }
+        }
     }
 
 
