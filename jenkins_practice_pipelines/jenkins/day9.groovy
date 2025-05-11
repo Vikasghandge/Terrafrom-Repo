78b45863e09a78edd92dbc45cc30ba891ca92970
vikasghandge <ghandgevikas804@gmail.com>
2025-05-11 18:33:34 +0530

    --- Auto Git Commit ---

--- jenkins_practice_pipelines/jenkins/day9.groovy
+++ jenkins_practice_pipelines/jenkins/day9.groovy
@@ -29,4 +29,5 @@ pipeline {
 
 // mvn lifecycle//
 mvn clean install -- // it will clean old build
-mvn compile -- // this will compile code into computer 
+mvn compile -- // this will compile code into computer language
+mvn package -- // 
