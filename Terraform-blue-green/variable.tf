2da5870dbe533606f082fc5bb14f30330cabf7dc
Vikas Ghandge <ghandgevikas804@gmail.com>
2025-06-15 11:36:51 +0530

    Update variable.tf

--- Terraform-blue-green/variable.tf
+++ Terraform-blue-green/variable.tf
@@ -1,1 +1,22 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "VPC for EC2 and ALB"
+}
+
+variable "public_subnets" {
+  description = "Public subnets for EC2 and ALB"
+  type        = list(string)
+}
+
+variable "ami_id" {
+  description = "AMI for EC2 instances"
+}
+
+variable "instance_type" {
+  description = " EC2 Instance Type"
+  default     = "t2.micro"
+}
 
